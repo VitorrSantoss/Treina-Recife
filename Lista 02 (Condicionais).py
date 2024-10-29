43141fb55d54911a8b65f68fd01a8780bc9637da
VitorrSantoss <166569430+VitorrSantoss@users.noreply.github.com>
2024-10-29 10:02:52 -0300

    Add files via upload

--- Lista 02 (Condicionais).py
+++ Lista 02 (Condicionais).py
@@ -1,25 +1,15 @@
 # EXERCICIO 01:
 num = int(input('Digite um número de (1 a 10): '))
-if num == 1:
-    print('1 -> Um')
-elif num == 2:
-    print('2 -> Dois')
-elif num == 3:
-    print('3 -> Três')
-elif num == 4:
-    print('4 -> Quatro')
-elif num == 5:
-    print('5 -> Cinco')
-elif num == 6:
-    print('6 -> Seis')
-elif num == 7:
-    print('7 -> Sete')
-elif num == 8:
-    print('8 -> Oito')
-elif num == 9:
-    print('9 -> Nove')
-elif num == 10:
-    print('10 -> Dez')
+if num == 1: print('1 -> Um')
+elif num == 2:print('2 -> Dois')
+elif num == 3:print('3 -> Três')
+elif num == 4:print('4 -> Quatro')
+elif num == 5:print('5 -> Cinco')
+elif num == 6: print('6 -> Seis')
+elif num == 7:  print('7 -> Sete')
+elif num == 8:print('8 -> Oito')
+elif num == 9:print('9 -> Nove')
+elif num == 10:print('10 -> Dez')
 else: print('Número Inválido.')
 
 # EXERCICIO 02:
@@ -34,13 +24,54 @@ elif soma == 10:
     print(' Sua média foi {} - APROVADO COM DISTINÇÃO!'.format(soma))
 
 # EXERCICIO 03:
+conta_sim = 0
+p1 = input('Telefonou para vítima? ').title()
+p2 = input('Esteve no local do crime? ').title()
+p3 = input('Mora perto da vítima ? ').title()
+p4 = input('Devia para vítima? ').title()
+p5= input('Já trabalhou com a vítima? ').title()
+if p1 == 'Sim':
+    conta_sim = conta_sim + 1
+if p2 == 'Sim':
+    conta_sim = conta_sim + 1
+if p3 == 'Sim':
+    conta_sim = conta_sim + 1
+if p4 == 'Sim':
+    conta_sim = conta_sim +1
+if p5 == 'Sim':
+    conta_sim = conta_sim + 1
+
+if conta_sim == 2:
+    print('SUSPEITA')
+elif conta_sim == 3 or conta_sim == 4:
+    print('CÚMPLICE')
+elif conta_sim == 5:
+    print('ASSASSINO')
+else: print('INOCENTE')
 
 # EXERCICIO 04:
+num = int(input('Digite um número até 4 algarismo: '))
+milhar = num // 1000 % 10
+centena = num // 100 % 10
+dezena = num // 10 % 10
+unidade = num // 1 % 10
+print('A milhar é {}\nA centena é {}\nA dezena é {}\nA unidade é {}'.format(milhar, centena, dezena, unidade))
+print('A soma dos algarismo {} + {} + {} + {} é {}'.format(milhar, centena, dezena, unidade, milhar+centena+dezena+unidade))
 
 # EXERCICIO 05:
+num = int(input('Digite um número: '))
+m_7 = num % 3
+m_3 = num % 7
+if m_7:
+    print('É multiplo de 7')
+elif m_3:
+    print('É multiplo de 3')
+elif m_3 == m_7:
+    print('É multiplo de 3 e 7')
+else: print('Não é multiplo')
+
 
 # EXERCICIO 06:
-
 idade = int(input('Digite sua idade: '))
 if 21 <= idade < 65:
     print('Maior de idade.')
@@ -51,6 +82,23 @@ elif idade >= 65:
     idade = int(input('Digite sua idade: '))
 
 # EXERCICIO 07:
+num = int(input('Digite um número: '))
+milhao = num // 10000 % 10
+milhar = num // 1000 % 10
+centena = num // 100 % 10
+dezena = num // 10 % 10
+unidade = num // 1 % 10
+
+print('Milhão', milhao)
+print('Milhar', milhar)
+print('Centena', centena)
+print('Dezena', dezena)
+print('Unidade', unidade)
+
+if milhao == unidade and milhar == dezena:
+    print('O número digitado é um PALÍNDROMO!')
+else: print('O número não é um PALÍNDROMO!')
+
 
 # EXERCICIO 08:
 peso = float(input('Digite seu peso em kilos: '))
--- TESTES TREINA.py
+++ TESTES TREINA.py
@@ -1,42 +1,19 @@
-num = int(input('Informe um número: '))
-u = num // 1 % 10
-d = num // 10 % 10
-c = num // 100 % 10
-m = num // 1000 % 10
-print('Analisando o número {}'.format(num))
-print('Unidade: {}'.format(u))
-print('Dezena: {}'.format(d))
-print('Centena: {}'.format(c))
-print('Milhar: {}'.format(m))
+num = int(input('Digite um número: '))
+milhao = num // 10000 % 10
+milhar = num // 1000 % 10
+centena = num // 100 % 10
+dezena = num // 10 % 10
+unidade = num // 1 % 10
 
+print('Milhão', milhao)
+print('Milhar', milhar)
+print('Centena', centena)
+print('Dezena', dezena)
+print('Unidade', unidade)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if milhao == unidade and milhar == dezena:
+    print('O número digitado é um PALÍNDROMO!')
+else: print('O número não é um PALÍNDROMO!')
 
 
 
